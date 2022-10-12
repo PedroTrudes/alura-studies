c27991d5cdf5aea1fde697e2710663ee1411df94
PedroTrudes <pedro.jtrudes@gmail.com>
2022-10-11 23:47:10 -0300

    Arquivos do projeto aluraStudies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Botao from './components/botao';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola mundo</h1>
+      <Botao />
+    </div>
+  );
+}
+
+export default App;
